90d2b39e383fbac10e3fa8e5c196c888db2a4159
Abhisek Datta <abhi.datta07@gmail.com>
2022-04-30 13:06:55 +0530

    First flask code

---
+++ app.py
@@ -0,0 +1,18 @@
+from distutils.log import debug
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/Abh')
+def Abh():
+    return 'My name is Abhisek'
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
